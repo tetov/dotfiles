92bb36ab02b3b7e5625067764928670b3c1af0c7
Sonya Mamurin <sonyamamurin@users.noreply.github.com>
2017-02-28 21:51:14 +0100

    Stow sh

---
+++ stow.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+command -v stow >/dev/null 2>&1 || { echo "Stow is required. Please install before running again." >&2; exit 1; }
+
+cd ~/tetov-dotfiles || { echo "Can't cd to ~/tetov-dotfiles." >&2; exit 1; }
+
+stow bash
+stow zsh
+stow vim
+stow lftp
+
+if [[ $OSTYPE == darwin* ]] ; then
+  stow karabiner-elements
+  stow hammerspoon
+elif [ "$OSTYPE" = "linux-gnu" ]; then
+	stow rmate
+else
+	echo "Unknown OS type"
+fi
+
+exit 0
