2b4bb778a26a777278a7b97029417008ead9eeda
tetov <sonya.mamurin@gmail.com>
2017-10-22 17:36:57 +0200

    Working installation script for linux (tested partly on ubuntu and         debian, more tests needed).

---
+++ install-linux.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Functions
+
+promptThenRun () {
+    local choice
+
+    echo
+
+    read -r -p "$1" choice
+
+    if [[ $choice =~ [yY](es)* ]]; then
+        $2
+    fi
+}
+
+aptInstall () {
+    sudo apt-get update
+
+    xargs sudo apt-get -y install <"$HOME"/tetov-dotfiles/apt-pkgs.txt
+}
+
+fzfInstall () {
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
+
+setZshShell () {
+    sudo chsh --shell="$(which zsh)" "$USER"
+}
+
+cloneDotfilesRepo () {
+    git clone https://github.com/tetov/tetov-dotfiles.git ~/tetov-dotfiles
+
+    promptThenRun 'Do you want to run the stow script? ' ~/tetov-dotfiles/stow.sh
+
+    promptThenRun 'Do you want to run the antigen & vundle script? ' ~/tetov-dotfiles/update-repos.sh
+
+}
+
+
+# INTERACTION
+
+promptThenRun 'Do you want to install apt packages? ' aptInstall
+
+
+promptThenRun 'Do you want to install fzf ' aptInstall
+promptThenRun 'Set zsh as login shell? ' fzfInstall
+promptThenRun 'Do you want to install apt packages? ' setZshShell
+promptThenRun 'Do you want clone dotfiles repo? ' cloneDotfilesRepo
