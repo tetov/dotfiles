6114184abff8a30fb465d2e334b40f328aadc0aa
Anton Tetov <anton@tetov.se>
2021-07-28 15:13:44 +0200

    Enable folding

--- stow-script.sh
+++ stow-script.sh
@@ -5,11 +5,9 @@ setopt extended_glob  # needed for glob after --stow
 STOW_DIR="$HOME/dotfiles"  # Should be repo dir
 TARGET_DIR=$HOME
 
-stow --dir="$STOW_DIR" \
+stow \
+  --dir="$STOW_DIR" \
   --target="$TARGET_DIR" \
-  --no-folding \
-  --ignore=deps \
-  --ignore=bin \
   --verbose \
   --stow ^(deps|bin)*/  # matches dirs but not those with names containing deps & bin
 
