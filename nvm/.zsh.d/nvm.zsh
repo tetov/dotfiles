be5bd1477fee2b9469251da27d517999cae72942
Anton Tetov <anton@tetov.se>
2021-08-19 17:59:14 +0200

    added nvm package

---
+++ nvm/.zsh.d/nvm.zsh
@@ -0,0 +1,4 @@
+# from https://github.com/nvm-sh/nvm#installing-and-updating
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
