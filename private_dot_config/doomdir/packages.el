e593e31a44a4980151267aedd82903b467f5d3c9
Anton Tetov <anton@tetov.se>
2022-05-01 19:55:13 +0200

    Update private_dot_config/doomdir/packages.el

--- private_dot_config/doomdir/packages.el
+++ private_dot_config/doomdir/packages.el
@@ -8,6 +8,7 @@
 ;(package! some-package)
 
 (package! git-auto-commit-mode)
+(package! atomic-chrome)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
