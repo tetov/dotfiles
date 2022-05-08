3103834cf80792e38567753fe72cc143ced6ad6c
Anton Tetov <anton@tetov.se>
2022-05-08 15:03:07 +0200

    Update private_dot_config/emacsconf/doomdir/packages.el Add selected

--- private_dot_config/emacsconf/doomdir/packages.el
+++ private_dot_config/emacsconf/doomdir/packages.el
@@ -9,6 +9,7 @@
 
 (package! git-auto-commit-mode)
 (package! atomic-chrome)
+(package! pocket-reader)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
