ffc0f97f39c815a81fb0aca32f42c82673fb848c
Anton Tetov <anton@tetov.se>
2022-05-27 12:01:50 +0200

    vterm and sync gnome settings using extension

--- .chezmoiscripts/run_after_gnome_extensions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gnome-extensions enable pixel-saver@deadalnix.me \
-    || echo 'You may need to reload GNOME Shell to recognise new extension by hitting Alt + F2 and entering "r"'
--- .chezmoiscripts/run_dump_dconf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dconf dump / > $(chezmoi source-path)/dconf.ini
