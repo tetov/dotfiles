10ac8ea9d3ee73bdb491583858aa00569175ff70
Anton Tetov <anton@tetov.se>
2022-06-04 17:57:54 +0200

    Update .chezmoiscripts/run_git_repo_settings.zsh Add private_dot_config/systemd/user/emacsd.service

--- .chezmoiscripts/run_git_repo_settings.zsh
+++ .chezmoiscripts/run_git_repo_settings.zsh
@@ -3,6 +3,8 @@
 cd $(chezmoi source-path)
 
 git config --local commit.gpgsign false
+git config --local core.sshCommand \
+    "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/dotfiles_repo_rsa"
 (( $+commands[pre-commit] )) && pre-commit install
 
 [[ -d ~/src/org ]] || git clone git@github.com:tetov/org ~/src/org
