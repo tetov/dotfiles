706e9865712febed1b9a1393e232d68e4089dbd4
Anton Tetov <anton@tetov.se>
2022-07-15 16:56:55 +0200

    Update .chezmoiscripts/run_after_00_org_dir_setup.sh

--- .chezmoiscripts/run_after_00_org_dir_setup.sh
+++ .chezmoiscripts/run_after_00_org_dir_setup.sh
@@ -7,7 +7,6 @@ if [ -d "$ORG_DIR/.git" ] ; then
     git config --local commit.gpgsign false
     git config --local core.sshCommand \
         "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/org_repo_rsa"
-    pre-commit install
 
     git config --local --bool branch.main.sync true
     git config --local --bool branch.main.syncNewFiles true
