d0e9e2981d4f094f8c0dec6acbe08b4e4e5dfa2e
Anton Tetov <anton@tetov.se>
2022-07-25 12:54:07 +0200

    protect sylpheed accountrc against storing passwords

--- .chezmoiscripts/run_after_00_chezmoi_dir_setup.sh
+++ .chezmoiscripts/run_after_00_chezmoi_dir_setup.sh
@@ -5,4 +5,7 @@ cd $(chezmoi source-path)
 git config --local commit.gpgsign false
 git config --local core.sshCommand \
     "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/dotfiles_repo_rsa"
+git config --local filter.sylpheed_remove_passwords.smudge \
+    "sed -Ee 's/^password=.*$/password=/'"
+
 pre-commit install
