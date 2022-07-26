dca8e1015d03463d0687467eda19b65ed0eb90dc
Anton Tetov <anton@tetov.se>
2022-07-26 10:51:50 +0200

    Update .chezmoiscripts/run_generate_zsh_completions.zsh

--- .chezmoiscripts/run_generate_zsh_completions.zsh
+++ .chezmoiscripts/run_generate_zsh_completions.zsh
@@ -3,5 +3,6 @@
 chezmoi completion zsh --output=$XDG_CONFIG_HOME/zsh/completions/_chezmoi
 
 (( $+commands[poetry] )) && poetry completions zsh > $XDG_CONFIG_HOME/zsh/completions/_poetry
+(( $+commands[op] )) && op completion zsh > $XDG_CONFIG_HOME/zsh/completions/_op
 
 exit 0
