6dbc0460c651cfbbb438767c364a62b37397a9dd
Anton Tetov <anton@tetov.se>
2022-08-03 09:49:46 +0200

    Update bin/executable_sync_emails.sh

--- bin/executable_sync_emails.sh
+++ bin/executable_sync_emails.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/sh
 
 MBSYNCRC="$XDG_CONFIG_HOME/mbsyncrc"
+LOCK_FILE="$XDG_CACHE_HOME/mu/xapian/flintlock"
 
-mbsync --config "$MBSYNCRC" --verbose --full lth
+CHANNELS="lth" # channel names separated with spaces
+
+mbsync --config "$MBSYNCRC" --verbose --full "$CHANNELS"
+
 # Run mu index, if that doesn't work (since mu4e has locked the database) run
 # mu4e-update-index
-mu index || emacsclient -e '(mu4e-update-index)'
+if [ ! -e "$LOCK_FILE" ] ; then
+    echo "Updating using mu"
+    mu index
+else
+    echo "Updating using mu4e"
+    emacsclient -e '(mu4e-update-index)'
+fi
