dbbd14f04dec1f28e2f511d9737735d478ef79fa
Anton Tetov <anton@tetov.se>
2022-08-05 12:08:07 +0200

    Update private_dot_config/emacsconf/doomdir/packages.el

--- private_dot_config/emacsconf/doomdir/packages.el
+++ private_dot_config/emacsconf/doomdir/packages.el
@@ -13,6 +13,10 @@
 (package! org-ref)
 (package! org-mime)
 
+(package! vdirel)
+(package! chezmoi)
+
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
