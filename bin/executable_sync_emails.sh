ca7ef98b0b01120c7b1897727eca29c98c62d292
Anton Tetov <anton@tetov.se>
2022-08-10 14:08:03 +0200

    Update bin/executable_sync_emails.sh

--- bin/executable_sync_emails.sh
+++ bin/executable_sync_emails.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/sh
 
-MBSYNC_LOCKFILE="$XDG_CACHE_HOME/sync_emails_lock"
-
 MBSYNCRC="$XDG_CONFIG_HOME/mbsyncrc"
 MU_LOCKFILE="$XDG_CACHE_HOME/mu/xapian/flintlock"
 
@@ -10,18 +8,6 @@ if [ $# -ne 0 ] ; then  # if positional args
 else
     GROUPS_OR_CHANNELS="--all"
 fi
-
-# from docstore.mik.ua/orelly/unix3/upt/ch36_27.htm
-until (umask 222; echo $$ >$MBSYNC_LOCKFILE) 2>/dev/null # test & set
-
-do
- # Optional message - show lockfile owner and creation time:
- set x $(ls -l $MBSYNC_LOCKFILE)
- echo "Waiting for user $4 (working since $7 $8 $9)..."
- sleep 5
-done
-
-echo "Aquired lock"
 
 # shellcheck disable=SC2086
 mbsync --config "$MBSYNCRC" --verbose $GROUPS_OR_CHANNELS
@@ -35,5 +21,3 @@ else
     echo "Updating using mu4e"
     emacsclient -e '(mu4e-update-index)'
 fi
-
-rm -f "$MBSYNC_LOCKFILE" # unlock
