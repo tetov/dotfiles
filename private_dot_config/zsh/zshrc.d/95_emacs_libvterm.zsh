d6ac9611d76ba617c91372896b548f1000df4eea
Anton Tetov <anton@tetov.se>
2022-08-14 00:05:04 +0200

    Add private_dot_config/zsh/zshrc.d/95_emacs_libvterm.zsh

---
+++ private_dot_config/zsh/zshrc.d/95_emacs_libvterm.zsh
@@ -0,0 +1,5 @@
+if [[ "$INSIDE_EMACS" = 'vterm' ]] \
+    && [[ -n ${EMACS_VTERM_PATH} ]] \
+    && [[ -f ${EMACS_VTERM_PATH}/etc/emacs-vterm-bash.sh ]]; then
+	source ${EMACS_VTERM_PATH}/etc/emacs-vterm-bash.sh
+fi
