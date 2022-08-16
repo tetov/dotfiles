0b9d0592dc5488c8b19aa924fe5080482e059ba9
Anton Tetov <anton@tetov.se>
2022-08-16 23:26:45 +0200

    Update private_dot_config/emacsconf/doomdir/packages.el

--- private_dot_config/emacsconf/doomdir/packages.el
+++ private_dot_config/emacsconf/doomdir/packages.el
@@ -13,6 +13,9 @@
 (package! org-ref)
 (package! org-mime)
 (package! vdirel)
+(package! org-ql)
+(package! org-ql)
+(package! helm-org-ql)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
