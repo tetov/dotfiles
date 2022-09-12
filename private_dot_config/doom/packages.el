9236a69fb23cdef03f48b685af075e48fc9278ac
Anton Tetov <anton@tetov.se>
2022-09-12 17:06:28 +0200

    Update private_dot_config/doom/packages.el

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -16,6 +16,7 @@
 (package! org-ql)
 (package! org-ql)
 (package! helm-org-ql)
+(package! org-wc)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
