6e280a29a0d430ddd390f15baa458080bd14877f
Anton Tetov <anton@tetov.se>
2022-09-23 00:39:12 +0200

    Update private_dot_config/doom/packages.el

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -7,16 +7,15 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
-(package! atomic-chrome)
 (package! pocket-reader)
 (package! backup-each-save)
 (package! org-ref)
 (package! org-mime)
 (package! vdirel)
 (package! org-ql)
-(package! org-ql)
 (package! helm-org-ql)
 (package! org-wc)
+(package! chezmoi)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
