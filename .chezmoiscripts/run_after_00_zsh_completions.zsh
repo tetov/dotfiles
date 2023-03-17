775217a0c44633bb237bf9a803d493c1ca2c2c1f
Anton Tetov <anton@tetov.se>
2023-03-17 17:59:40 +0100

    Remove .chezmoiscripts/00_fix_control_auto_home_dot_local_permissions.sh Remove .chezmoiscripts/00_zsh_completions.zsh Add .chezmoiscripts/00_zsh_completions.zsh Update .chezmoiscripts/10_gnome_extensions.sh Update .chezmoiscripts/30_gnome_sync_dump.sh Update .chezmoiscripts/00_mu_init.sh Update .chezmoiscripts/10_update-desktop-database.sh Update .chezmoiscripts/20_gnome_sync_load.sh Remove .chezmoiscripts/30_fisher_update.sh Update .chezmoiscripts/40_doom_upgrade.sh Update .chezmoiscripts/50_doom_sync.sh Remove .chezmoiscripts/60_plugin_upgrades.sh Add .chezmoiscripts/60_plugin_upgrades.sh Update .local/share/applications/org-protocol.desktop Change attributes of .chezmoiscripts/00_chezmoi_dir_setup.sh Change attributes of .config/vim/vimrc

--- .chezmoiscripts/run_after_00_zsh_completions.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-chezmoi completion zsh --output=$XDG_CONFIG_HOME/zsh/completions/_chezmoi
-
-(( $+commands[poetry] )) && poetry completions zsh > $XDG_CONFIG_HOME/zsh/completions/_poetry
-(( $+commands[op] )) && op completion zsh > $XDG_CONFIG_HOME/zsh/completions/_op
-
-exit 0
