63972003ef15b390762c55dc24bee2cbfe1a6b40
Anton Tetov <anton@tetov.se>
2023-03-27 15:54:49 +0200

    Update .profile Update .config/zsh/zshrc.d/50_plugin_manager.zsh

--- private_dot_config/zsh/zshrc.d/50_plugin_manager.zsh
+++ private_dot_config/zsh/zshrc.d/50_plugin_manager.zsh
@@ -1,5 +1,8 @@
 # ZIM
 ZIM_HOME=$XDG_DATA_HOME/zsh/zim
+zstyle ':zim' disable-version-check yes
+zstyle ':zim:completion' dumpfile ${ZIM_HOME}/zcompdump
+zstyle ':completion::complete:*' cache-path ${XDG_CACHE_HOME}/zsh/zcompcache
 # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
 if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
   source ${ZIM_HOME}/zimfw.zsh init -q
