4e363a2bfbc0c288494214e41eee5028ced1fccc
Anton Tetov <anton@tetov.se>
2023-04-24 11:18:55 +0200

    Update private_dot_config/zsh/p10k.zsh

--- private_dot_config/zsh/p10k.zsh
+++ private_dot_config/zsh/p10k.zsh
@@ -100,9 +100,9 @@
     # cpu_arch              # CPU architecture
     # time                  # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
-    # public_ip             # public IP address
+    public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
+    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
