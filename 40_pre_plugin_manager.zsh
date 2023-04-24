cb6d14b648fb2ab19969413f85c7f86c583a88f5
Anton Tetov <anton@tetov.se>
2023-04-24 10:59:48 +0200

    Add 40_pre_plugin_manager.zsh Update private_dot_config/zsh/dot_zimrc Update private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh Add private_dot_config/zsh/p10k.zsh

---
+++ 40_pre_plugin_manager.zsh
@@ -0,0 +1,6 @@
+PURE_CMD_MAX_EXEC_TIME=60
+PURE_GIT_PULL=0
+
+# print() {
+#   [ 0 -eq $# -a "prompt_pure_precmd" = "${funcstack[-1]}" ] || builtin print "$@";
+# }
--- private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh
+++ private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh
@@ -1,6 +1,9 @@
-PURE_CMD_MAX_EXEC_TIME=60
-PURE_GIT_PULL=0
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
 
-print() {
-  [ 0 -eq $# -a "prompt_pure_precmd" = "${funcstack[-1]}" ] || builtin print "$@";
-}
+# To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+[[ ! -f $ZDOTDIR/p10k.zsh ]] || source $ZDOTDIR/p10k.zsh
