b8072b3b9868d7f4f35617d93b3cc436af27bc87
Anton Tetov <anton@tetov.se>
2023-04-24 10:43:28 +0200

    Update private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh

--- private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh
+++ private_dot_config/zsh/exact_zshrc.d/45_pre_plugins.zsh
@@ -1,6 +1,5 @@
 PURE_CMD_MAX_EXEC_TIME=60
 PURE_GIT_PULL=0
-PURE_PROMPT_SYMBOL='%f%F{red}#%f %F{magenta}❯' # prefix prompt when root
 
 print() {
   [ 0 -eq $# -a "prompt_pure_precmd" = "${funcstack[-1]}" ] || builtin print "$@";
