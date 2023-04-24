a7774fe522a830bfb3dc473487357ede64267c72
Anton Tetov <anton@tetov.se>
2023-04-24 11:13:42 +0200

    dot_zimrc -> zimrc

--- private_dot_config/zsh/exact_zshrc.d/50_plugin_manager.zsh
+++ private_dot_config/zsh/exact_zshrc.d/50_plugin_manager.zsh
@@ -1,10 +1,13 @@
 # ZIM
 ZIM_HOME=$XDG_CACHE_HOME/zsh/zim
+ZIM_CONFIG_FILE=$ZDOTDIR/zimrc
+
 zstyle ':zim' disable-version-check yes
 zstyle ':zim:completion' dumpfile ${ZIM_HOME}/zcompdump
 zstyle ':completion::complete:*' cache-path ${XDG_CACHE_HOME}/zsh/zcompcache
+
 # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE} ]]; then
   source ${ZIM_HOME}/zimfw.zsh init -q
 fi
 source ${ZIM_HOME}/init.zsh
