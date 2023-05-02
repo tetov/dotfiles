6c5cb0a7adbaa430fddef8427a065df2734ab114
Anton Tetov <anton@tetov.se>
2023-05-02 20:50:21 +0200

    Update .config/zsh/zshrc.d/60_tmux_window_name.zsh

--- private_dot_config/zsh/exact_zshrc.d/60_tmux_window_name.zsh
+++ private_dot_config/zsh/exact_zshrc.d/60_tmux_window_name.zsh
@@ -1,5 +1,7 @@
 tmux-window-name() {
-    ($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+    if [[ -v TMUX_PLUGIN_MANAGER_PATH ]] ; then
+        ($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+    fi
 }
 
 add-zsh-hook chpwd tmux-window-name
