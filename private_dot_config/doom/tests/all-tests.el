777622b279b9a31bbc4920660413906af1431164
Anton Tetov <anton@tetov.se>
2023-06-27 14:16:48 +0200

    Add .config/doom/tests/Makefile Update .config/doom/tests/all-tests.el Update .config/doom/tests/teditor-backup.el

--- private_dot_config/doom/tests/all-tests.el
+++ private_dot_config/doom/tests/all-tests.el
@@ -4,7 +4,8 @@
 ;; version, prefer to use the newer (typically the uncompiled) version.
 (setq load-prefer-newer t)
 
-(add-to-list 'load-path (file-name-directory load-file-name))
+(add-to-list 'load-path (file-name-directory (or load-file-name
+                                                 (buffer-file-name))))
 (require 'teditor-backup)
 
 (ert-run-tests-batch-and-exit)
