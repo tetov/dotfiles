fb1b385ac3f4cf00f6e163879406de0cc417e610
Anton Tetov <anton@tetov.se>
2023-08-25 13:26:45 +0200

    Update .profile Add .config/gtk-3.0/settings.ini Update .config/vim/vimrc Remove .config/zsh/zshrc.d/60_tmux_window_name.zsh Update .config/zsh/zimrc

--- private_dot_config/zsh/exact_zshrc.d/60_tmux_window_name.zsh
+++
@@ -1,7 +0,0 @@
-tmux-window-name() {
-    if [[ -v TMUX_PLUGIN_MANAGER_PATH ]] ; then
-        ($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
-    fi
-}
-
-add-zsh-hook chpwd tmux-window-name
