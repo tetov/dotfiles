6300ce42b2e92ad83d791e8a9cad09344ffc63e0
Anton Tetov <anton@tetov.se>
2023-08-25 13:37:06 +0200

    Update .config/doom/packages.el

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -2,7 +2,7 @@
 ;;; $DOOMDIR/packages.el To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
-(package! org :pin "ca873f7fe47546bca19821f1578a6ab95bf5351c5890aca")
+;;(package! org :pin "ca873f7fe47546bca19821f1578a6ab95bf5351c5890aca")
 ;; (unpin! org-roam) ;; for orb and org-roam-ui
 (package! org-roam-ui)
 
