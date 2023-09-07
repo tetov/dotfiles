2401a895769e101bd158a5e2e4cb6a5d1145b96b
Anton Tetov <anton@tetov.se>
2023-09-07 14:57:48 +0200

    Update .config/zsh/p10k.zsh

--- private_dot_config/zsh/p10k.zsh
+++ private_dot_config/zsh/p10k.zsh
@@ -76,7 +76,7 @@
     # nnn                     # nnn shell (https://github.com/jarun/nnn)
     # lf                      # lf shell (https://github.com/gokcehan/lf)
     # xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
-    vim_shell               # vim shell indicator (:sh)
+    # vim_shell               # vim shell indicator (:sh)
     # midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     # nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vpn_ip                # virtual private network indicator
