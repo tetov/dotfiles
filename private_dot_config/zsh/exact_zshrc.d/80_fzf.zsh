c1615703baefaba561e0f8df6393b5b85ffdd51d
Anton Tetov <anton@tetov.se>
2023-09-07 14:28:40 +0200

    Update .config/zsh/zshrc.d/80_fzf.zsh

--- private_dot_config/zsh/exact_zshrc.d/80_fzf.zsh
+++ private_dot_config/zsh/exact_zshrc.d/80_fzf.zsh
@@ -5,10 +5,5 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_COMPLETION_TRIGGER='~~'
 # export FZF_DEFAULT_OPTS='-m --height 50% --border'
 
-function load_additional_zsh_files() {
-    [[ -r /usr/share/fzf/shell/key-bindings.zsh ]] && . /usr/share/fzf/shell/key-bindings.zsh
-    [[ -r /usr/share/zsh/site-functions/fzf ]] && . /usr/share/fzf/shell/key-bindings.zsh
-}
-
-# functions that runs after zsh-vi-mode...
-zvm_after_init_commands+=(load_additional_zsh_files)
+[[ -r /usr/share/fzf/shell/key-bindings.zsh ]] && . /usr/share/fzf/shell/key-bindings.zsh
+[[ -r /usr/share/zsh/site-functions/fzf ]] && . /usr/share/zsh/site-functions/fzf
