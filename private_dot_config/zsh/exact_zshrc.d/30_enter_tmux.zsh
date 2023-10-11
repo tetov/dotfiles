086877ff2623aa66a8356c36948157d5e165f02c
Anton Tetov <anton@tetov.se>
2023-10-11 22:54:32 +0200

    Add .config/systemd/user/rclone_bisync_nextcloud@.service Add .config/systemd/user/rclone_bisync_nextcloud@.timer Add .config/systemd/user/timers.target.wants/rclone_bisync_nextcloud@org.timer Update .config/zsh/zshrc.d/30_enter_tmux.zsh

--- private_dot_config/zsh/exact_zshrc.d/30_enter_tmux.zsh
+++ private_dot_config/zsh/exact_zshrc.d/30_enter_tmux.zsh
@@ -1,6 +1,9 @@
 (( $+commands[tmux] ))                         \
     && [[ ! -v TMUX ]]                         \
     && [[ ! -v INSIDE_EMACS ]]                 \
+    && [[ ! -v WT_SESSION ]]                   \
+    && [[ $TERM_PROGRAM != "vscode" ]]         \
+    && [[ $TERM != "dumb" ]]                   \
     && [[ $XDG_SESSION_DESKTOP != "sway" ]]    \
     && tmux new-session -A -s $USER-tmux-session -t $USER-tmux-session-group
 
