ac1c8697e1e6dd7dba7c4065bad4af97a2aa4621
Anton Tetov <anton@tetov.se>
2023-11-19 19:27:21 +0100

    Update .config/zsh/.zshrc Update .config/zsh/zshrc.d/99_zle_emacs.zsh

--- private_dot_config/zsh/exact_zshrc.d/99_zle_emacs.zsh
+++ private_dot_config/zsh/exact_zshrc.d/99_zle_emacs.zsh
@@ -1,7 +1,15 @@
 # man zshzle
 bindkey -e
 
-# home
-bindkey "^[[1~" beginning-of-line
-# end
-bindkey "^[[4~" end-of-line
+ZKBD_FILE=$ZDOTDIR/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}
+
+if [[ -e "$ZKBD_FILE" ]] ; then
+    . "$ZKBD_FILE"
+    [[ -n ${key[Home]} ]] && bindkey "${key[Home]}" beginning-of-line
+    [[ -n ${key[End]} ]] && bindkey "${key[End]}" end-of-line
+    [[ -n ${key[Delete]} ]] && bindkey "${key[Delete]}" delete-char
+else
+    echo "Run zkbd to setup keybindings for terminal emulator."
+    echo
+    echo '$ZDOTDIR ; autoload zkbd ; zkbd'
+fi
